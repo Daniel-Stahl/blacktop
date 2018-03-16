a9f886b72a12e9db33ae344c930cb0f9a1caf274
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2018-03-15 22:39:36 -0700

    added tap gesture to avatar and bgImage && take photo

--- blacktop/Controller/CafeProfileSetupVC.swift
+++ blacktop/Controller/CafeProfileSetupVC.swift
@@ -23,16 +23,42 @@ class CafeProfileSetupVC: UIViewController {
     @IBOutlet weak var instagram: UITextField!
     @IBOutlet weak var saveBtn: UIButton!
     
-    
+    var imagePicker: UIImagePickerController!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        saveBtn.bindToKeyboard()
+        bgImageTap()
+        avatarTap()
         
     }
-
+    
+    func bgImageTap() {
+        let tapImage = UITapGestureRecognizer(target: self, action: #selector(selectImage(sender:)))
+        tapImage.numberOfTapsRequired = 1
+        bgImage.addGestureRecognizer(tapImage)
+    }
+    
+    func avatarTap() {
+        let tapImage = UITapGestureRecognizer(target: self, action: #selector(selectImage(sender:)))
+        tapImage.numberOfTapsRequired = 1
+        avatar.addGestureRecognizer(tapImage)
+    }
+    
+    
+    @objc func selectImage(sender: UITapGestureRecognizer) {
+        imagePicker = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            imagePicker.cameraCaptureMode = .photo
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func saveBtnPressed(_ sender: Any) {
         DataService.instance.cafeProfile(uid: (Auth.auth().currentUser?.uid)! ,name: cafeName.text!, description: cafeDescription.text!, address: cafeAddress.text!, hours: cafeHours.text!, website: website.text!, facebook: facebook.text!, twitter: twitter.text!, instagram: instagram.text!) { (success) in
             if success {
