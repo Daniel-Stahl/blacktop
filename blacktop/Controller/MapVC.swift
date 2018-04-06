6fcab523471cd60540e495b92f3cbe8e589f8227
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2018-04-06 13:13:53 -0700

    trying to center map on pins when touched

--- blacktop/Controller/MapVC.swift
+++ blacktop/Controller/MapVC.swift
@@ -11,21 +11,31 @@ import MapKit
 import CoreLocation
 import Firebase
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullUpView: UIView!
+    @IBOutlet weak var mapViewBottomConstraint: NSLayoutConstraint!
     
     let locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         locationManager.delegate = self
-        
+        //centerMapOnUserLocation()
         confirmAuthStatus()
         
+        touchPin()
         cafePins()
+    }
+    
+    func touchPin() {
+        let touch = UITapGestureRecognizer(target: self, action: #selector(cafePins))
+        touch.delegate = self
+        mapView.addGestureRecognizer(touch)
     }
 
     @IBAction func profileBtnPressed(_ sender: Any) {
@@ -40,27 +50,52 @@ class MapVC: UIViewController {
         }
     }
     
+    
     //If someone doesnt put an address catch it.
-    func cafePins() {
+    @objc func cafePins() {
         DataService.instance.REF_CAFES.observe(.value) { (snapshot) in
             guard let snapshot = snapshot.children.allObjects as? [DataSnapshot] else { return }
             for data in snapshot {
                 let address = data.childSnapshot(forPath: "address").value as? String
 
                 let geoCoder = CLGeocoder()
-                    geoCoder.geocodeAddressString(address!, completionHandler: { (place, error) in
-                        let location = place?.first?.location
+                
+                geoCoder.geocodeAddressString(address!, completionHandler: { (place, error) in
+                    let location = place?.first?.location
+                    let annotation = MKPointAnnotation()
+                    var pinDrop = [location]
+                    for location in pinDrop {
+                    annotation.coordinate = (location?.coordinate)!
+                    self.mapView.addAnnotation(annotation)
                         
-                        let annotation = MKPointAnnotation()
-                        var pinDrop = [location]
-                        for location in pinDrop {
-                        annotation.coordinate = (location?.coordinate)!
-                        self.mapView.addAnnotation(annotation)
-
                     }
+                    let span = MKCoordinateSpanMake(0.5, 0.5)
+                    let region = MKCoordinateRegion(center: annotation.coordinate, span: span)
+                        self.mapView.setRegion(region, animated: true)
                 })
             }
         }
+    }
+    
+    
+    
+    func annimateViewUp() {
+        mapViewBottomConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func centerButtonPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2, regionRadius * 2)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 }
 
